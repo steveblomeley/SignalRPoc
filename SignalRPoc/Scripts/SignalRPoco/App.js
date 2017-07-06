011f749b7c11fdba4b138cab64674f9a1536d2a5
steve <>
2017-07-06 15:22:29 +0100

    Wire up a mechanism to: - Post edits using Ajax so that main listing screen isn't refreshed after each post - Remove item from list (ie: remove it from the DOM) after a successful post - Display (then hide) the result of each ajax post in an alert at the top of the main listing page

--- SignalRPoc/Scripts/SignalRPoco/App.js
+++ SignalRPoc/Scripts/SignalRPoco/App.js
@@ -1,1 +1,19 @@
-﻿
+﻿jQuery.fn.showAlertMessage = function (message) {
+    return $(this).empty().append(message).removeClass("hidden").show().delay(3000).hide(2000);
+};
+
+function PostSuccess(data) {
+    $("#alert-success").showAlertMessage(data.Message);
+    var editorId = "#editor-for-model-" + data.Id;
+    $(editorId).prev("tr").remove();
+    $(editorId).remove();
+    if ($("[id^=editor-for-model-]").length === 0) {
+        $("#no-more-data").removeClass("hidden");
+    }
+}
+
+function PostFail(data) {
+    $("#alert-error").showAlertMessage(data.responseJSON.Message);
+    var editorId = "#editor-for-model-" + data.responseJSON.Id;
+    $(editorId).empty();
+}
